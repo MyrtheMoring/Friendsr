2d0ac271e4699a806b367ee27a0b68de1de1f934
Myrthe Moring <myrthe.moring@student.uva.nl>
2019-02-21 17:41:27 +0200

    working app

--- app/src/main/java/com/example/friendsr/MainActivity.java
+++ app/src/main/java/com/example/friendsr/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.friendsr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 import java.util.ArrayList;
+import java.io.Serializable;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +19,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Friend bert = new Friend("Bert", "Nice guy", 1);
+        if (savedInstanceState == null) {
+            friends = new ArrayList<>();
+            makeFriends();
+        }
+      else {
+            friends = savedInstanceState.getParcelableArrayList("friends");
+        }
+        setAdapter();
+    }
 
-        friends.add(bert);
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("friends", friends);
+    }
 
-//        getResources().getIdentifier()
+    public void makeFriends() {
+        friends.add(new Friend("Arya", "Arya .. ", this.getResources().getIdentifier("arya", "drawable", this.getPackageName())));
+        friends.add(new Friend("Cersei", "", this.getResources().getIdentifier("cersei" , "drawable", this.getPackageName())));
+        friends.add(new Friend("Daenerys", "", this.getResources().getIdentifier("daenerys" , "drawable", this.getPackageName())));
+        friends.add(new Friend("Jaime", "", this.getResources().getIdentifier("jaime" , "drawable", this.getPackageName())));
+        friends.add(new Friend("Jon", "", this.getResources().getIdentifier("jon" , "drawable", this.getPackageName())));
+        friends.add(new Friend("Jorah", "Jorah .. ", this.getResources().getIdentifier("jorah" , "drawable", this.getPackageName())));
+        friends.add(new Friend("Margaery", "", this.getResources().getIdentifier("margaery" , "drawable", this.getPackageName())));
+        friends.add(new Friend("Melisandre", "", this.getResources().getIdentifier("melisandre" , "drawable", this.getPackageName())));
+        friends.add(new Friend("Sannsa", "", this.getResources().getIdentifier("sansa" , "drawable", this.getPackageName())));
+        friends.add(new Friend("Tyrion", "", this.getResources().getIdentifier("tyrion" , "drawable", this.getPackageName())));
+    }
+
+    public void setAdapter() {
+        FriendsAdapter adapter = new FriendsAdapter(this, R.layout.grid_item, friends);
+        GridView view = findViewById(R.id.mainContainer);
+        view.setAdapter(adapter);
+        view.setOnItemClickListener(new GridItemClickListener());
+    }
+
+    private class GridItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Friend clickedFriend = (Friend) parent.getItemAtPosition(position);
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            intent.putExtra("clicked_friend", (Serializable) clickedFriend);
+            startActivity(intent);
+        }
     }
 
 
---
+++ app/src/main/java/com/example/friendsr/ProfileActivity.java
@@ -0,0 +1,59 @@
+package com.example.friendsr;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private Friend retrievedFriend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Intent intent = getIntent();
+        retrievedFriend = (Friend) intent.getSerializableExtra("clicked_friend");
+
+        ImageView profilepicture = (ImageView) findViewById(R.id.profilepicture);
+        profilepicture.setImageResource(retrievedFriend.getDrawableId());
+
+        TextView profilename = (TextView) findViewById(R.id.profilename);
+        profilename.setText(retrievedFriend.getName());
+
+        TextView profilebio = (TextView) findViewById(R.id.profilebio);
+        profilebio.setText(retrievedFriend.getBio());
+
+        RatingBar personRatingBar = findViewById(R.id.profilerating);
+        personRatingBar.setOnRatingBarChangeListener(new RatingBarChangeListener());
+
+        SharedPreferences prefs = getSharedPreferences("settings", MODE_PRIVATE);
+        float r = prefs.getFloat(retrievedFriend.getName(), (float) 0.0);
+        Log.d("FLOAT", ""+r);
+
+        if (r == (float) 0.0) {
+            personRatingBar.setRating((float) 0.0);
+        }
+        else {
+            personRatingBar.setRating(r);
+        }
+
+    }
+
+    private class RatingBarChangeListener implements RatingBar.OnRatingBarChangeListener {
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+            SharedPreferences prefs = getSharedPreferences("settings", MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putFloat(retrievedFriend.getName(), rating);
+            editor.apply();
+            retrievedFriend.setRating(rating);
+        }
+    }
+}
